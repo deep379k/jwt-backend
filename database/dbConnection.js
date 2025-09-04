6e8a17ebb77a78a2abeb032a84d3c981d44777e8
DK <deepanshu739k@gmail.com>
2025-09-04 18:04:33 +0530

    updated

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 
-export const connection = () => {
-    mongoose.connect(process.env.MONGO_URI,{
+export const connection = async () => {
+    await mongoose.connect(process.env.MONGO_URI,{
         dbName: "MERN_AUTHENTICATION", // iska use hm database ka naam set krne ke liye krte hai
     }).then(() => {
         console.log("Database connected successfully");
